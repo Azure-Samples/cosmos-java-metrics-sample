aae1e5e3031e6694f968d38a085d73431adbada1
Richa Gaur <richagaur@microsoft.com>
2024-11-27 16:38:46 +0530

    inital console metrics integration

---
+++ src/main/java/com/azure/cosmos/metrics/sample/common/Pet.java
@@ -0,0 +1,13 @@
+package com.azure.cosmos.metrics.sample.common;
+
+public class Pet {
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    private String givenName;
+}
